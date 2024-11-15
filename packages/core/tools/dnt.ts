e8acc04bcb15af9b9f0eebaa33c0db03e24e1c68
Stepan Novozhilov <hit2hat@icloud.com>
2024-11-15 13:24:49 +0300

    feat(core): improve dnt configuration

--- packages/core/tools/dnt.ts
+++ packages/core/tools/dnt.ts
@@ -1,20 +1,34 @@
 import { build, emptyDir } from 'jsr:@deno/dnt@0.41.3';
 import denoJson from '../deno.json' with { type: 'json' };
 
-const root = import.meta.dirname + '/';
-await emptyDir(root + '../npm');
+const root = import.meta.dirname + '/../';
+await emptyDir(root + 'npm');
 
 const packageJson = {
     name: denoJson.name,
     version: denoJson.version,
+    description: 'Framework for creating a multi-entity API backend',
+    license: denoJson.license,
+    repository: {
+        type: 'git',
+        url: 'git+https://github.com/aeternus-capital/theatrum.git',
+    },
+    bugs: {
+        url: 'https://github.com/aeternus-capital/theatrum/issues',
+    },
 };
 
 await build({
     shims: {
         deno: true,
     },
-    outDir: root + '../npm',
+    packageManager: 'yarn',
+    outDir: root + 'npm',
     package: packageJson,
-    importMap: root + '../deno.json',
-    entryPoints: [ root + '../src/_.ts' ],
+    importMap: root + 'deno.json',
+    entryPoints: [ root + 'src/_.ts' ],
+    postBuild(): void {
+        Deno.copyFileSync(root + 'README.md', root + 'npm/README.md');
+        Deno.copyFileSync(root + 'LICENSE', root + 'npm/LICENSE.md');
+    },
 });
